4ad6d118a600275ef07a6489e2658f3c07a56934
Isadora Araújo <isadoraraujo13@gmail.com>
2022-09-16 15:21:47 -0300

    atividade em grupo para fiap

---
+++ src/atividadeUm.ts
@@ -0,0 +1,15 @@
+console.log("_______________\t\t_______________\t\t_______________\t\t_______________\t\t_______________");
+console.log("| Tabuada do 1 |\t| Tabuada do 2 |\t| Tabuada do 3 |\t| Tabuada do 4 |\t| Tabuada do 5 |");
+console.log("_______________\t\t_______________\t\t_______________\t\t_______________\t\t_______________");
+
+for(let i = 1; i <= 10; i++) {
+    console.log(`| 1 x ${i} = ${i*1} \t|\t| 2 x ${i} = ${i*2}  \t|\t| 3 x ${i} = ${i*3}  \t|\t| 4 x ${i} = ${i*4}  \t|\t| 5 x ${i} = ${i*5}  |`);
+}
+
+console.log("_______________\t\t_______________\t\t_______________\t\t_______________\t\t_______________");
+console.log("| Tabuada do 6 |\t| Tabuada do 7 |\t| Tabuada do 8 |\t| Tabuada do 9 |\t| Tabuada do 10 |");
+console.log("_______________\t\t_______________\t\t_______________\t\t_______________\t\t_______________");
+
+for(let i = 1; i <= 10; i++) {
+    console.log(`| 6 x ${i} = ${i*6} \t|\t| 7 x ${i} = ${i*7} \t|\t| 8 x ${i} = ${i*8} \t|\t| 9 x ${i} = ${i*9} \t|\t| 10 x ${i} = ${i*10} |`)
+}
